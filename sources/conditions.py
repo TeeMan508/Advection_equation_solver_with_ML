0e9e82275f5e5c96cbf1d7ec322e6102ff4a8ed1
Dits Daniil <daniil.dits@gmail.com>
2023-04-12 21:46:15 +0300

    add option geometry_type for generate robust data

--- sources/conditions.py
+++ sources/conditions.py
@@ -21,7 +21,10 @@ def generate_grid(grid_resolution: int, grid_length: int) -> grids.Grid:
 
 
 
-def generate_initial_conditions(grid: grids.Grid, height_list: np.array, width_list: np.array) -> dict:
+def generate_initial_conditions(grid: grids.Grid, height_list: np.array, width_list: np.array, geometry_type="square":str) -> dict:
+
+"choose geometry_type like \"square\" or \"gaussian\" - quasi-random square"
+
     def make_square(x, height=1.0, center=0.25, width=0.1):
         nx = x.shape[0]
         c = np.zeros_like(x)
@@ -37,13 +40,60 @@ def generate_initial_conditions(grid: grids.Grid, height_list: np.array, width_l
 
         return np.array(c_list)
 
-    x, _ = grid.get_mesh()
-    c_init = make_multi_square(x, height_list=height_list, width_list=width_list)
-    initial_state = {
-        'concentration': c_init.astype(np.float32),  # tensorflow code expects float32
-        'x_velocity': np.ones(c_init.shape, np.float32) * 1.0,
-        'y_velocity': np.zeros(c_init.shape, np.float32)
-    }
+    def make_gaussian(x, height=1.0, center=0.25, width=0.1):
+        """
+        Args:
+        x: Numpy array. Shape should be (nx, 1) or (nx,)
+        height: float, peak concentration
+        center: float, relative center position in 0~1
+        width: float, relative width in 0~0.5
+
+        Returns:
+        Numpy array, same shape as `x`
+        """
+        nx = x.shape[0]
+        x_max = x.max()
+        center *= x_max
+        width *= x_max
+        c = height * np.exp(-(x - center) ** 2 / width ** 2)
+        return c
+
+    def make_multi_gaussian(x, height_list, width_list):
+
+        c_list = []
+        for height in height_list:
+            for width in width_list:
+                c_temp = make_gaussian(x, height=height, width=width)
+                c_list.append(c_temp)
+
+        return np.array(c_list)
+
+    if geometry_type == "square":
+        x, _ = grid.get_mesh()
+        c_init = make_multi_square(x, height_list=height_list, width_list=width_list)
+        initial_state = {
+            'concentration': c_init.astype(np.float32),  # tensorflow code expects float32
+            'x_velocity': np.ones(c_init.shape, np.float32) * 1.0,
+            'y_velocity': np.zeros(c_init.shape, np.float32)
+        }
+
+    elif geometry_type == "gaussian":
+        np.random.seed(41)
+        height_list_guass = np.random.uniform(0.1, 0.5, size=10)
+        width_list_guass = np.random.uniform(1 / 16, 1 / 4, size=3)
+
+        c_init_guass = make_multi_gaussian(
+            x_coarse,
+            height_list=height_list_guass,
+            width_list=width_list_guass
+        )
+
+        c_init_guass.shape  # (sample, x, y)
+        initial_state = {
+            'concentration': c_init_guass.astype(np.float32),  # tensorflow code expects float32
+            'x_velocity': np.ones(c_init_guass.shape, np.float32) * 1.0,
+            'y_velocity': np.zeros(c_init_guass.shape, np.float32)
+        }
 
     return initial_state
 
